5cc1bd7a17435bcf6963f7a44d1cd3ab6d44ffbc
luke <luke@userflux.co>
2023-11-17 17:19:14 +1100

    Add identify method

--- index.js
+++ index.js
@@ -25,9 +25,16 @@ class UserFlux {
             return;
         }
         
+        // sanity check userId
         const userId = parameters.userId;
-        if (userId == 'null' || userId == '' || userId == 'undefined') {
+        if (typeof userId !== 'string' || userId == 'null' || userId == '' || userId == 'undefined') {
             console.error('Invalid userId passed to track method');
+            return;
+        }
+
+        // sanity check event
+        if (typeof parameters.event !== 'string' || parameters.event == 'null' || parameters.event == '' || parameters.event == 'undefined') {
+            console.error('Invalid event passed to track method');
             return;
         }
 
@@ -50,6 +57,37 @@ class UserFlux {
         };
 
         await this.sendRequest('event/ingest', payload);
+    }
+
+    async identify(parameters) {
+        // need to check if `parameters` is an object and contains `userId` and `properties` properties
+        if (!parameters || typeof parameters !== 'object' || !parameters.userId || !parameters.properties) {
+            console.error('Invalid parameters passed to identify method');
+            return;
+        }
+
+        // sanity check userId
+        const userId = parameters.userId;
+        if (typeof userId !== 'string' || userId == 'null' || userId == '' || userId == 'undefined') {
+            console.error('Invalid userId passed to track method');
+            return;
+        }
+
+        // sanity check properties
+        const properties = parameters.properties;
+        if (typeof properties !== 'object') {
+            console.error('Invalid properties passed to identify method');
+            return;
+        }
+
+        const payload = {
+            userId: userId,
+            anonymousId: null,
+            properties: properties,
+            deviceData: null
+        };
+
+        await this.sendRequest('profile', payload);
     }
 
 }
